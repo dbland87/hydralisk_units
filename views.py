f1f3f2d5b9bc482b533d9ffdce405353cc0a51a9
dbland87 <dbland87@gmail.com>
2017-05-01 19:51:55 -0600

    Added units endpoint

--- views.py
+++ views.py
@@ -1,6 +1,29 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
+from django.db.migrations import serializer
+from rest_framework import request, status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.parsers import JSONParser
 
-# Create your views here.
+from units.models import Unit
+from units.serializers import UnitSerializer
+from user_auth.models import Bag
+
+
+class RequestUnits(APIView):
+    def post(self, request, format=None):
+        #Check if user exists
+        data = JSONParser().parse(request)
+        bag_id = data['user']['bag']
+        if Bag.objects.filter(id = bag_id).exists():
+            #Grab all units in that bag
+            units = Unit.objects.filter(bag=bag_id)
+            serializer = UnitSerializer(units, many=True)
+            return Response({"bag" : serializer.data}, status=status.HTTP_200_OK)
+        else:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+
+
+
